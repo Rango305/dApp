3e4f1ee00ff05102c80da9b17f451c06640b0d3d
Rango <82874690+Rango305@users.noreply.github.com>
2023-01-28 00:24:00 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,57 @@
+const MoodContractAddress = "0xXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+const MoodContractABI = [
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_mood",
+        type: "string",
+      },
+    ],
+    name: "setMood",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getMood",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+let MoodContract;
+let signer;
+
+const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
+
+provider.send("eth_requestAccounts", []).then(() => {
+  provider.listAccounts().then((accounts) => {
+    signer = provider.getSigner(accounts[0]);
+    MoodContract = new ethers.Contract(
+      MoodContractAddress,
+      MoodContractABI,
+      signer
+    );
+  });
+});
+
+async function getMood() {
+  const getMoodPromise = MoodContract.getMood();
+  const Mood = await getMoodPromise;
+  document.getElementById("console").innerText = `The mood is ${Mood}.`;
+  console.log(Mood);
+}
+
+async function setMood() {
+  const mood = document.getElementById("mood").value;
+  const setMoodPromise = MoodContract.setMood(mood);
+  await setMoodPromise;
+}
